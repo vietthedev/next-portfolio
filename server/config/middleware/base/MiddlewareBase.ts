783b62078e2d049e78795d10d0054d97bd902e70
S-a-l-a-d <xa.lach@yahoo.com>
2018-06-25 16:03:34 +0700

    Add compression middleware and remove unnecessary packages

--- server/config/middleware/base/MiddlewareBase.ts
+++ server/config/middleware/base/MiddlewareBase.ts
@@ -1,4 +1,5 @@
 import bodyParser from "body-parser";
+import compression from "compression";
 import express from "express";
 
 import RouteBase from "../../routes/base/RouteBase";
@@ -14,6 +15,7 @@ export default class MiddlewareBase {
     app.use(ExpressSession.configuration());
     app.use(Lusca.configuration());
     app.use(MethodOverride.configuration());
+    app.use(compression());
 
     app.use(new RouteBase().routes);
 
